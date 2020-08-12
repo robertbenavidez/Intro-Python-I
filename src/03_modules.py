8954e719e9993ba2356be3de300d77b65b4d485b
Robert Benavidez <murabitbenavidez@gmail.com>
2020-08-12 19:24:02 -0400

    corrections to modules and tuples

--- src/03_modules.py
+++ src/03_modules.py
@@ -18,7 +18,9 @@ import getpass
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
 
-print(sys.argv[0])
+
+for i in sys.argv:
+    print(i)
 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
